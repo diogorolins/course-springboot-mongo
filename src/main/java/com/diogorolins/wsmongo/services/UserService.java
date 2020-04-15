fb8470a41a40c1e024702a31e28ccdb6dc41166c
= <=>
2020-04-15 12:12:01 -0300

    user by id

--- src/main/java/com/diogorolins/wsmongo/services/UserService.java
+++ src/main/java/com/diogorolins/wsmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.diogorolins.wsmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.diogorolins.wsmongo.domains.User;
 import com.diogorolins.wsmongo.repositories.UserRepository;
+import com.diogorolins.wsmongo.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -15,7 +17,11 @@ public class UserService {
 	private UserRepository repository;
 	
 	public List<User> findAll(){
-		return repository.findAll();
-		
+		return repository.findAll();		
+	}
+	
+	public User findById(String id) {
+		Optional<User>  obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
