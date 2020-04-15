121d76d65da8c7bbb9c82cdb6dd182cea8c3db73
= <=>
2020-04-15 15:20:48 -0300

    Entity post

--- src/main/java/com/diogorolins/wsmongo/config/Instantiation.java
+++ src/main/java/com/diogorolins/wsmongo/config/Instantiation.java
@@ -1,31 +1,46 @@
 package com.diogorolins.wsmongo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.diogorolins.wsmongo.domains.Post;
 import com.diogorolins.wsmongo.domains.User;
+import com.diogorolins.wsmongo.repositories.PostRepository;
 import com.diogorolins.wsmongo.repositories.UserRepository;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
 
 	@Autowired
-	private UserRepository repository; 
+	private UserRepository userRepository; 
+	
+	@Autowired
+	private PostRepository postRepository; 
 	
 	@Override
 	public void run(String... args) throws Exception {
 		
-		repository.deleteAll();
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
+		userRepository.deleteAll();
+		postRepository.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		repository.saveAll(Arrays.asList(maria, alex, bob));
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
+		
+		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
 }
